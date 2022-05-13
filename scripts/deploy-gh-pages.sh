14fccf661847c8df30fd2feed6b4fa59e2201f62
Chris Tunbridge <chris@octorage.com>
2022-05-13 09:18:13 -0600

    Make script push changes

--- scripts/deploy-gh-pages.sh
+++ scripts/deploy-gh-pages.sh
@@ -4,4 +4,5 @@ NODE_ENV=production npm run build && \
 cp ./dist/index.html ./dist/404.html && \
 git add -A ./dist && \
 git commit -m "Build" && \
-git subtree push --prefix dist origin gh-pages
+git subtree push --prefix dist origin gh-pages && \
+git push
