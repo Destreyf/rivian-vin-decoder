2c2d555b69f8a8381701a677d928089c47a42509
Chris Tunbridge <chris@octorage.com>
2022-05-13 09:03:09 -0600

    Build

--- scripts/deploy-gh-pages.sh
+++ scripts/deploy-gh-pages.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
-NODE_ENV=production npm run build
-
+NODE_ENV=production npm run build && \
+git add -A ./dist && \
+git commit -am "Build" && \
 git subtree push --prefix dist origin gh-pages
